1e17c6f5d592f3ac7ec4f4d8d1108b3119f6d9e5
Manoj Kumar Paladugu <paladugumanojkumar@gmail.com>
2024-07-23 00:28:28 +0530

    fix: Got UART print working

---
+++ G491RET6/Core/Include/Main.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
